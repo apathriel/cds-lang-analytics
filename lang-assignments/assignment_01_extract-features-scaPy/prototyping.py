35bbf1224d5ae8b01c524893b2c877dc51f73dd3
Gabriel Høst Andersen <gabrielandersen1@gmail.com>
2024-02-21 16:36:05 +0100

    Update dependencies. Add loop, work on encoding check

--- lang-assignments/assignment_01_extract-features-scaPy/prototyping.py
+++ lang-assignments/assignment_01_extract-features-scaPy/prototyping.py
@@ -2,101 +2,113 @@ import pandas as pd
 import spacy
 import os
 import sys
+from charset_normalizer import from_bytes, from_path
+import chardet
 
 #probably gonna nest for loops for iterating all the files in subfolders
 
+# load model, language class initialized through setup.sh
 nlp = spacy.load("en_core_web_md")
 
 input_folder_file_path = os.path.join("data", "input")
+input_file_for_testing = os.path.join(input_folder_file_path, "b1", "0103.b1.txt")
 
-input_file_for_testing = os.path.join(input_folder_file_path, "a1", "0100.a1.txt")
 
-input_folders = os.listdir(input_folder_file_path)
+# NEED TO CHECK FOR ENCODING 
 
-with open (input_file_for_testing, "r", encoding="utf-8") as file:
+with open (input_file_for_testing, "r", encoding="ISO-8859-1") as file:
     text = file.read()
+
+
 
 doc = nlp(text)
 
-""" for token in doc:
-    print(token.i, token.text, token.pos_) """
+# CHECK FOR ENCODING, ASSUMING UTF-8, ATTEMPT DETERMINE ENCODING
+def open_text_file(filepath):
+    try:
+        with open (filepath, "r", encoding="utf-8") as file:
+            return file.read()
+    except UnicodeDecodeError:
+        with open (filepath, "rb") as file:
+            get_encoding = chardet.detect(file.read()['encoding'])
+        try:
+            with open (filepath, encoding=get_encoding) as file:
+                return file.read()
+        except:
+            print("ERROR")
+            break
 
-""" for ent in doc.ents:
-    print(ent.text, ent.label_) """
+def folder_loop(input_folder_path):
+    folder_count = 0
+    input_folders = os.listdir(input_folder_path)
+    # loop through all folders
+    for folder in input_folders:
+        # get all sub folders of current folder in loop
+        input_sub_folders = os.listdir(os.path.join(input_folder_path, folder))
+        # loop through all sub folders
+        for sub_folder in input_sub_folders:
+            folder_count += 1
+    print(folder_count)
 
-entities = []
-
-for ent in doc.ents:
-    entities.append(ent.text)
-
-# set "removes" duplicates, maybe ensures no duplicates through its initialization is more correct
-print(f'These are all the unique entities! {set(entities)}')
-
-# let's count the number of adjectives (and find the rel frequency)
-
-adjective_count = 0
-for token in doc:
-    if token.pos_ == "ADJ":
-        adjective_count += 1
-
-adj_rel_freq = (adjective_count/len(doc)) * 10000
-
-print(f'The relative frequency of adjectives are {adj_rel_freq}')
+def count_occurances_named_entity(document):
+    person_count = 0
+    org_count = 0
+    loc_count = 0
+    for ent in document.ents:
+        try:
+            match ent.label_:
+                case 'PERSON':
+                    person_count += 1
+                case 'ORG':
+                    org_count += 1
+                case 'LOC':
+                    loc_count += 1
+        except TypeError:
+            return
+    print(person_count, org_count)
 
 # ideas include making an initial list, looping through each and calculating score
-def count_occurances(document):
-    noun_count = verb_count = adj_count = adv_count = 0
-    for token in doc:
-        match token.pos_:
-            case "NOUN":
-                noun_count += 1
-            case "VERB":
-                verb_count += 1
-            case "ADJ":
-                adj_count += 1
-            case "ADV":
-                adv_count += 1
-            case _:
-                continue
-    return adj_count
 
 def calculate_relative_frequency(document, count, per_how_many_words):
-    return (count/len(doc)) * per_how_many_words
+    return round((count/len(doc)) * per_how_many_words, 2)
 
-#dictionaries are ordered as per Python v 3.7
-def count_occurances_comprehension(document):
-    NOUN, VERB, ADJ, ADV = 'noun_count', 'verb_count', 'adj_count', 'adv_count'
+# rewrite to take a list and create based on that
+def count_occurances_type(document):
+    # initialize variables to ensure consistency in references
+    NOUN, VERB, ADJ, ADV, OTHER_COUNT, LOOP_COUNT = 'noun_count', 'verb_count', 'adj_count', 'adv_count', 0, 0
     token_types = {
-        'noun_count': 0,
-        'verb_count': 0,
-        'adj_count': 0,
-        'adv_count': 0
+        NOUN: 0,
+        VERB: 0,
+        ADJ: 0,
+        ADV: 0
         }
 
 # can validate dict structure, probably do variable parameter (if possible :O)
-    for token in doc:
+    for token in document:
         try:
             match token.pos_:
                 case "NOUN":
-                    token_types['noun_count'] += 1
+                    token_types[NOUN] += 1
+                    LOOP_COUNT += 1
                 case "VERB":
-                    token_types['verb_count'] += 1
+                    token_types[VERB] += 1
+                    LOOP_COUNT += 1
                 case "ADJ":
-                    token_types['adj_count'] += 1
+                    token_types[ADJ] += 1
+                    LOOP_COUNT += 1
                 case "ADV":
-                    token_types['adv_count'] += 1
+                    token_types[ADV] += 1
+                    LOOP_COUNT += 1
                 case _:
+                    OTHER_COUNT += 1
+                    LOOP_COUNT += 1
                     continue
         except TypeError:
             print("[ERROR] Value is not a number")
         except KeyError:
             print("[ERROR] Key does not exist :(")
-
-    
+    print(f'[COMPLETE] {LOOP_COUNT} tokens have been looped through. {OTHER_COUNT} were not of the specified types.')
     return [calculate_relative_frequency(doc, count, 10000) for count in token_types.values()]
-    
-    
 
-frequency_list = count_occurances_comprehension(doc)
+frequency_list = count_occurances_type(doc)
 print(f'This is the frequency list: {frequency_list}')
-
--- lang-assignments/assignment_01_extract-features-scaPy/setup.sh
+++ lang-assignments/assignment_01_extract-features-scaPy/setup.sh
@@ -1,4 +1,4 @@
-python -m spacy download eng_core_web_md
-
+# adding pyarrow purely because the error message was annoying
 python3 -m pip install -r requirements.txt
 
+python -m spacy download en_core_web_md
