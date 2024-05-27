a877298e4eefd3afed5f36fa282d0f192b772a6f
Gabriel Høst Andersen <94848510+apathriel@users.noreply.github.com>
2024-05-27 14:45:51 +0200

    Refactor data processing utilities module, separate logic in file loading, and add cross-validation output. Add load as jotlib.

--- assignments/text_classification_benchmarks/src/data_processing_utilities.py
+++ assignments/text_classification_benchmarks/src/data_processing_utilities.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from typing import Any, Tuple
+from typing import Any, List, Tuple
 from statistics import mean
 
-from joblib import dump
+from joblib import dump, load
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -14,23 +14,57 @@ logger = get_logger(__name__)
 
 
 def load_labeled_data_as_df(path_to_data: Path) -> pd.DataFrame:
+    """
+    Load labeled data from a CSV file into a pandas DataFrame.
+
+    Parameters:
+        path_to_data (Path): The path to the CSV file.
+
+    Returns:
+        pd.DataFrame: The labeled data as a pandas DataFrame.
+    """
     return pd.read_csv(path_to_data)
 
-def save_cross_validated_scores_to_txt(scores, output_dir: Path, file_name: str):
+
+def save_cross_validated_scores_to_txt(
+    scores: List[float], output_dir: Path, file_name: str, decimals: int = 3
+):
+    """
+    Save cross-validated scores to a text file.
+
+    Parameters:
+        scores (List[float]): The list of cross-validated scores.
+        output_dir (Path): The directory where the output file will be saved.
+        file_name (str): The name of the output file (without extension).
+        decimals (int, optional): The number of decimal places to round the scores to. Defaults to 3.
+
+    Raises:
+        Exception: If there is an error while saving the scores.
+
+    """
     try:
         output_dir.mkdir(parents=True, exist_ok=True)
         with open(output_dir / f"{file_name}.txt", "w") as file:
             file.write("Cross-validated scores:\n")
             for i, score in enumerate(scores, 1):
-                file.write(f"Fold {i}: {score}\n")
-            file.write(f"Mean score: {mean(scores)}\n")
+                file.write(f"Fold {i}: {round(score, decimals)}\n")
+            file.write(f"Mean score: {round(mean(scores), decimals)}\n")
     except Exception as e:
         logger.error(f"Failed to save cross-validated scores: {e}")
+
 
 def save_classification_report_to_txt(
     classification_report: str, output_dir: Path, file_name: str
 ) -> None:
-    """Saves the classification report to a text file."""
+    """
+    Saves the classification report to a text file.
+
+    Parameters:
+        classification_report (str): The classification report to be saved.
+        output_dir (Path): The directory where the text file will be saved.
+        file_name (str): The name of the text file.
+
+    """
     try:
         output_dir.mkdir(parents=True, exist_ok=True)
         full_file_path = output_dir / f"{file_name}.txt"
@@ -41,10 +75,38 @@ def save_classification_report_to_txt(
         logger.error(f"Failed to save classification report: {e}")
 
 
+def load_object_from_joblib(file_path: Path) -> Any:
+    """
+    Loads an object from a joblib file.
+
+    Parameters:
+        file_path (Path): The path to the joblib file.
+
+    Returns:
+        Any: The loaded object.
+
+    Raises:
+        Exception: If there is an error loading the object.
+    """
+    try:
+        return load(file_path)
+    except Exception as e:
+        logger.error(f"Failed to load object from {file_path}: {e}")
+
+
 def save_object_as_joblib(
     object_to_save: Any, output_dir: Path, file_stem: str, object_name: str
 ) -> None:
-    """Saves an object to a joblib file."""
+    """
+    Saves an object to a joblib file.
+
+    Parameters:
+        object_to_save (Any): The object to be saved.
+        output_dir (Path): The directory where the file will be saved.
+        file_stem (str): The stem of the file name.
+        object_name (str): The name of the object being saved.
+
+    """
     try:
         output_dir.mkdir(parents=True, exist_ok=True)
         file_name = output_dir / f"{file_stem}_{object_name}.joblib"
@@ -53,6 +115,7 @@ def save_object_as_joblib(
     except Exception as e:
         logger.error(f"Failed to save {object_name}: {e}")
 
+
 def load_and_split_training_data(
     data: pd.DataFrame,
     text_col: str,
@@ -60,13 +123,27 @@ def load_and_split_training_data(
     train_test_size: float = 0.2,
     seed: int = 24,
 ) -> Tuple[pd.Series, pd.Series, pd.Series, pd.Series]:
+    """
+    Load and split the training data into train and test sets.
+
+    Parameters:
+        data (pd.DataFrame): The input DataFrame containing the data.
+        text_col (str): The name of the column containing the text data.
+        label_col (str): The name of the column containing the label data.
+        train_test_size (float, optional): The proportion of the data to include in the test split. Defaults to 0.2.
+        seed (int, optional): The random seed for reproducibility. Defaults to 24.
+
+    Returns:
+        Tuple[pd.Series, pd.Series, pd.Series, pd.Series]: A tuple containing the train-test split of the text and label data.
+    """
     logger.info(
         f"Splitting data into train and test sets with test size {train_test_size} and seed {seed}."
     )
     X = data[text_col]
     y = data[label_col]
-    return train_test_split(X, y, test_size=train_test_size, random_state=seed)
-
+    return train_test_split(
+        X, y, test_size=train_test_size, random_state=seed, stratify=y
+    )
 
 
 def prepare_data_for_model_training(
@@ -77,7 +154,20 @@ def prepare_data_for_model_training(
     train_test_size: float = 0.2,
     seed: int = 24,
 ) -> Tuple[csr_matrix, csr_matrix, pd.Series, pd.Series]:
-    """Prepares data for training and testing."""
+    """
+    Prepares data for model training and testing.
+
+    Parameters:
+        data (pd.DataFrame): The input data containing text and label columns.
+        text_col (str): The name of the column containing the text data.
+        label_col (str): The name of the column containing the label data.
+        vectorizer (TfidfVectorizer): The vectorizer used to transform text data into feature vectors.
+        train_test_size (float, optional): The proportion of data to use for testing. Defaults to 0.2.
+        seed (int, optional): The random seed for splitting the data. Defaults to 24.
+
+    Returns:
+        Tuple[csr_matrix, csr_matrix, pd.Series, pd.Series]: A tuple containing the feature vectors for training and testing, and the corresponding label series for training and testing.
+    """
     logger.info("Preparing data for model training...")
 
     if text_col not in data.columns or label_col not in data.columns:
--- assignments/text_classification_benchmarks/src/neural_network.py
+++ assignments/text_classification_benchmarks/src/neural_network.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 from statistics import mean
-from typing import Union
+from typing import Any, Dict, Optional, Union
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
@@ -21,42 +21,102 @@ from utilities import get_logger
 logger = get_logger(__name__)
 
 
+from typing import Dict, Any, Union, Optional
+import pandas as pd
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from pathlib import Path
+import logging
+
+logger = logging.getLogger(__name__)
+
 def train_neural_network_classifier_model(
-    X_train: Union[pd.DataFrame, np.ndarray], y_train: Union[pd.Series, np.ndarray]
-) -> MLPClassifier:
-    logger.info("Training neural network classifier...")
-    return MLPClassifier(
-        activation="logistic", hidden_layer_sizes=(20,), max_iter=1000, random_state=42
-    ).fit(X_train, y_train)
-
-
-def neural_network_news_classification_pipeline(
-    data: pd.DataFrame,
-    text_col: str,
-    label_col: str,
-    vectorizer: Union[CountVectorizer, TfidfVectorizer],
-    report_path: Path,
-    model_path: Path,
-    train_test_size: float = 0.2,
-    seed: int = 24,
+    output_dir: Path,
+    X_train: Union[pd.DataFrame, np.ndarray],
+    y_train: Union[pd.Series, np.ndarray],
+    clf_parameters: Dict[str, Any],
+    use_grid_search: bool = False,
+    grid_search_params: Optional[Dict[str, Any]] = None,
     cross_validate: bool = False,
     cv_fold: int = 10,
-):
-    X_train_feats, X_test_feats, y_train, y_test = prepare_data_for_model_training(
-        data, text_col, label_col, vectorizer, train_test_size, seed
-    )
-    classifier = train_neural_network_classifier_model(X_train_feats, y_train)
-    y_pred = classifier.predict(X_test_feats)
+) -> MLPClassifier:
+    """
+    Trains a neural network classifier model. Saves the trained model, vectroizer, and classification report.
+
+    Parameters:
+        output_dir (Path): The output directory to save the trained model and results.
+        X_train (Union[pd.DataFrame, np.ndarray]): The input features for training.
+        y_train (Union[pd.Series, np.ndarray]): The target labels for training.
+        clf_parameters (Dict[str, Any]): The parameters for the MLPClassifier model.
+        use_grid_search (bool, optional): Whether to perform grid search for hyperparameters. Defaults to False.
+        grid_search_params (Optional[Dict[str, Any]], optional): The grid search parameters. Defaults to None.
+        cross_validate (bool, optional): Whether to perform cross-validation. Defaults to False.
+        cv_fold (int, optional): The number of cross-validation folds. Defaults to 10.
+
+    Returns:
+        MLPClassifier: The trained MLPClassifier model.
+    """
+    logger.info("Training neural network classifier...")
+
+    if use_grid_search and grid_search_params is not None:
+        logger.info("Performing grid search for hyperparameters...")
+        # Define the model
+        mlp = MLPClassifier(max_iter=1000, random_state=42, early_stopping=True)
+
+        # Set up the grid search
+        clf = GridSearchCV(mlp, grid_search_params, n_jobs=-1, cv=5, verbose=3)
+
+        # Fit the model and find the best hyperparameters
+        clf.fit(X_train, y_train)
+
+        logger.info(f"Best parameters found: {clf.best_params_}")
+
+        best_estimator = clf.best_estimator_
+    else:
+        unpacked_clf_parameters = {
+            key: value[0] for key, value in clf_parameters.items()
+        }
+        best_estimator = MLPClassifier(
+            **unpacked_clf_parameters,
+            max_iter=1000,
+            random_state=42,
+            early_stopping=True,
+        ).fit(X_train, y_train)
 
     if cross_validate:
         logger.info(f"Cross-validating with {cv_fold} folds...")
-        scores = cross_val_score(classifier, X_train_feats, y_train, cv=cv_fold)
+        scores = cross_val_score(best_estimator, X_train, y_train, cv=cv_fold)
         logger.info(
-            f"Cross-validation complete. Cross-validated mean score: {round(mean(scores), 2)}"
+            f"Cross-validation complete. Cross-validated mean score: {round(np.mean(scores), 2)}"
         )
-        save_cross_validated_scores_to_txt(
-            scores, report_path, "neural_network_cross_validated_scores"
-        )
+        save_cross_validated_scores_to_txt(scores, output_dir, "neural_network_cross_validated_scores")
+
+    return best_estimator
+
+
+def neural_network_news_classification_pipeline(
+    vectorizer: Union[CountVectorizer, TfidfVectorizer],
+    report_path: Path,
+    model_path: Path,
+    classifier: MLPClassifier, 
+    X_test_feats: Union[pd.DataFrame, np.ndarray],
+    y_test: Union[pd.Series, np.ndarray],
+) -> None:
+    """
+    Runs a news classification pipeline using a neural network classifier.
+
+    Parameters:
+        vectorizer (Union[CountVectorizer, TfidfVectorizer]): The vectorizer used to transform the input data.
+        report_path (Path): The path to save the classification report.
+        model_path (Path): The path to save the trained model.
+        classifier (MLPClassifier): The neural network classifier.
+        X_test_feats (Union[pd.DataFrame, np.ndarray]): The features of the test data.
+        y_test (Union[pd.Series, np.ndarray]): The labels of the test data.
+
+    """
+
+    y_pred = classifier.predict(X_test_feats)
 
     save_classification_report_to_txt(
         classification_report=classification_report(y_test, y_pred),
@@ -96,12 +156,43 @@ def main():
         max_features=100,
     )
 
-    # Run the neural network pipeline, training the model and saving the report and model
-    neural_network_news_classification_pipeline(
+    # Prepare the data for model training
+    X_train_feats, X_test_feats, y_train, y_test = prepare_data_for_model_training(
         data=news_dataset,
         text_col="text",
         label_col="label",
         vectorizer=vectorizer,
+        train_test_size=0.20,
+        seed=24,
+    )
+
+    # Define the grid of hyperparameters to search
+    parameter_space = {
+        "hidden_layer_sizes": [(50, 100, 50)],
+        "activation": ["relu"],
+        "solver": ["adam"],
+        "alpha": [0.05],
+        "learning_rate": ["constant"],
+    }
+
+    # Train the neural network classifier
+    mlp_classifier = train_neural_network_classifier_model(
+        output_dir=report_data_path,
+        X_train=X_train_feats,
+        y_train=y_train,
+        clf_parameters=parameter_space,
+        use_grid_search=False,
+        grid_search_params=None,
+        cross_validate=True,
+        cv_fold=10,
+    )
+
+    # Run the neural network pipeline, training the model and saving the report and model
+    neural_network_news_classification_pipeline(
+        vectorizer=vectorizer,
+        classifier=mlp_classifier,
+        X_test_feats=X_test_feats,
+        y_test=y_test,
         report_path=report_data_path,
         model_path=model_data_path,
     )
