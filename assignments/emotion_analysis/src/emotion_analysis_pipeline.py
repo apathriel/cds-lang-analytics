a37f4b2ef9770a2fbe04abd827a87dca18f37f8e
Gabriel Høst Andersen <94848510+apathriel@users.noreply.github.com>
2024-05-28 00:55:59 +0200

    Refactor emotion_analysis_pipeline.py, improve code structure and readability

--- assignments/emotion_analysis/src/emotion_analysis_pipeline.py
+++ assignments/emotion_analysis/src/emotion_analysis_pipeline.py
@@ -1,24 +1,26 @@
-import click
-import os
 from pathlib import Path
+from typing import Optional
 
-from data_manipulation_utils import (
-    save_df_to_csv,
-    load_csv,
-    convert_column_to_data_type,
-)
+import click
 import pandas as pd
 from tqdm import tqdm
-from transformers import pipeline
+from transformers import pipeline, Pipeline
+
+from cli_decorator import cli_options
+from data_manipulation_utils import (
+    export_df_as_csv,
+    load_csv_as_df,
+    convert_column_to_data_type,
+)
 
 from logger_utils import get_logger
 
-LOGGER = get_logger(__name__)
+logger = get_logger(__name__)
 
 
 def emotion_analysis_pipeline(
     df: pd.DataFrame,
-    classifier: pipeline,
+    classifier: Pipeline,
     raw_text_column: str,
     emotion_column_title: str,
     score_column_title: str,
@@ -42,71 +44,27 @@ def emotion_analysis_pipeline(
 
 
 @click.command()
-@click.option(
-    "--input_csv_path",
-    "-i",
-    help="Path to the input CSV file relative to this scripts parent folder",
-    prompt=True,
-    required=True,
-)
-@click.option(
-    "--save_results_to_csv",
-    "-s",
-    help="Save the results to a new CSV file",
-    default=True,
-    type=bool,
-)
-@click.option(
-    "--output_csv_path", "-o", help="Path to the output CSV file", default=None
-)
-@click.option(
-    "--raw_text_column",
-    "-rtc",
-    help="Name of the column containing raw text data",
-    default="Sentence",
-)
-@click.option(
-    "--hf_model",
-    "-m",
-    help="Name of the Hugging Face model to use for classification",
-    default="j-hartmann/emotion-english-distilroberta-base",
-)
-@click.option(
-    "--emotion_column_title",
-    "-ect",
-    help="Name of the column to store the predicted emotion",
-    default="Emotion",
-)
-@click.option(
-    "--score_column_title",
-    "-sct",
-    help="Name of the column to store the prediction score",
-    default="Score",
-)
+@cli_options
 def main(
-    input_csv_path,
-    save_results_to_csv,
-    output_csv_path,
-    raw_text_column,
-    hf_model,
-    emotion_column_title,
-    score_column_title,
-):
+    input_csv_path: str,
+    save_results_to_csv: bool,
+    output_csv_path: Optional[str],
+    raw_text_column: str,
+    hf_model: str,
+    emotion_column_title: str,
+    score_column_title: str,
+) -> None:
     # Initialize CSV paths for input and output
-    input_data_path = Path(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", input_csv_path)
+    input_csv_path = (
+        f"{input_csv_path}.csv"
+        if not input_csv_path.endswith(".csv")
+        else input_csv_path
     )
+    input_data_path = Path(__file__).parent / ".." / "in" / input_csv_path
     output_data_path = (
-        Path(
-            os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), "..", output_csv_path
-            )
-        )
-        if output_csv_path
-        else None
+        Path(__file__).parent / ".." / output_csv_path if output_csv_path else None
     )
 
-    print(input_data_path)
     # Load the Hugging Face model. Initialize the text classifier pipeline.
     text_classifier = pipeline(
         task="text-classification",
@@ -116,7 +74,7 @@ def main(
     )
 
     # Load CSV file
-    df = load_csv(input_data_path)
+    df = load_csv_as_df(input_data_path)
     # Convert the column to the appropriate data type
     df = convert_column_to_data_type(df, "Sentence", str)
     # Run the emotion analysis pipeline
@@ -129,7 +87,7 @@ def main(
     )
     # Save the results to a new CSV file
     if save_results_to_csv:
-        save_df_to_csv(df, output_data_path)
+        export_df_as_csv(df, output_data_path)
 
 
 if __name__ == "__main__":
