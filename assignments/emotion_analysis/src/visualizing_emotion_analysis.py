6b54d65dbc915411c062a9fc0f2d71b05bec8ffe
apathriel <gabrielandersen1@gmail.com>
2024-05-28 03:45:33 +0200

    Add modularity through helper functions.

--- assignments/emotion_analysis/src/visualizing_emotion_analysis.py
+++ assignments/emotion_analysis/src/visualizing_emotion_analysis.py
@@ -1,5 +1,9 @@
 import math
 from pathlib import Path
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
+import pandas as pd
 
 from logger_utils import get_logger
 from data_manipulation_utils import (
@@ -7,11 +11,32 @@ from data_manipulation_utils import (
     get_column_value_counts_by_group_as_percentage,
     convert_column_to_data_type,
 )
-import matplotlib.pyplot as plt
-import pandas as pd
 
 
 logger = get_logger(__name__)
+
+
+def create_subplots(
+    num_of_subplots: int, num_subplots_columns: int
+) -> Tuple[plt.Figure, List[plt.Axes]]:
+    num_rows = math.ceil(num_of_subplots / num_subplots_columns)
+    fig, axs = plt.subplots(num_rows, num_subplots_columns, figsize=(20, 5 * num_rows))
+    axs = axs.flatten()
+    for ax in axs[num_of_subplots:]:
+        fig.delaxes(ax)
+    return fig, axs
+
+
+def save_or_show_plot(
+    figure: plt.Figure,
+    output_dir: Path,
+    plot_output_title: str = None,
+    plot_output_format: str = "png",
+) -> None:
+    output_dir.mkdir(parents=True, exist_ok=True)
+    save_path = output_dir / f"{plot_output_title}.{plot_output_format}"
+    figure.savefig(fname=save_path)
+    logger.info(f"Plot saved as {save_path}")
 
 
 def visualize_relative_emotion_distribution_by_season(
@@ -25,16 +50,7 @@ def visualize_relative_emotion_distribution_by_season(
 ) -> None:
     # Calculate the number of rows needed for the grid
     num_of_subplots = len(normalized_counts_by_category.index.levels[0])
-    num_rows = math.ceil(num_of_subplots / num_subplots_columns)
-
-    # Create a subplot for each emotion in a grid
-    fig, axs = plt.subplots(num_rows, num_subplots_columns, figsize=(20, 5 * num_rows))
-
-    # Flatten the axes array
-    axs = axs.flatten()
-
-    for ax in axs[num_of_subplots:]:
-        fig.delaxes(ax)
+    fig, axs = create_subplots(num_of_subplots, num_subplots_columns)
 
     fig.suptitle(plot_title.capitalize(), fontsize=16)
     fig.subplots_adjust(hspace=1)
@@ -59,10 +75,7 @@ def visualize_relative_emotion_distribution_by_season(
         plt.setp(axs[i].xaxis.get_majorticklabels(), rotation=45)
 
     if plot_output_title:
-        output_dir.mkdir(parents=True, exist_ok=True)
-        save_path = output_dir / f"{plot_output_title}.{plot_output_format}"
-        plt.savefig(fname=save_path)
-        logger.info(f"Plot saved as {save_path}")
+        save_or_show_plot(fig, output_dir, plot_output_title, plot_output_format)
     else:
         plt.show()
 
@@ -77,18 +90,8 @@ def visualize_emotion_flunctuations_across_seasons(
     plot_output_format: str = "png",
 ) -> None:
     num_of_subplots = len(normalized_counts_across_timeseries.index)
-    num_rows = math.ceil(num_of_subplots / num_subplots_columns)
-
+    fig, axs = create_subplots(num_of_subplots, num_subplots_columns)
     # Create a subplot for each emotion in a 2-column grid
-    fig, axs = plt.subplots(num_rows, num_subplots_columns, figsize=(20, 5 * num_rows))
-
-    # Flatten the axes array
-    axs = axs.flatten()
-
-    for ax in axs[num_of_subplots:]:
-        fig.delaxes(ax)
-
-    # Get all labels from the X axis
 
     fig.suptitle(plot_title.capitalize(), fontsize=16)
     fig.subplots_adjust(hspace=1)
@@ -118,10 +121,7 @@ def visualize_emotion_flunctuations_across_seasons(
 
     # Handle plot output
     if plot_output_title:
-        output_dir.mkdir(parents=True, exist_ok=True)
-        save_path = output_dir / f"{plot_output_title}.{plot_output_format}"
-        plt.savefig(fname=save_path)
-        logger.info(f"Plot saved as {save_path}")
+        save_or_show_plot(fig, output_dir, plot_output_title, plot_output_format)
     else:
         plt.show()
 
